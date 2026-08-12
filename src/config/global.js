c6f08052de7e916b6bdfbc7a64823bd872fc8922
Henry Álvarez Astudillo <halvarezast@gmail.com>
2026-08-12 18:51:48 -0500

    VinculacionCFA

--- src/config/global.js
+++ src/config/global.js
@@ -274,7 +274,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/12310016_CF01_DU.pdf',
       },
       {
         icono: 'fas fa-download',
